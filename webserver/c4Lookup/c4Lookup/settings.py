d9abad210b46bd7a7546470903f4b46f863d7b80
Dominic Gaiero <dpgaiero@gmail.com>
2019-06-30 17:19:02 -0700

    update default renderer

--- webserver/c4Lookup/c4Lookup/settings.py
+++ webserver/c4Lookup/c4Lookup/settings.py
@@ -86,9 +86,9 @@ REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': [
       'rest_framework.authentication.TokenAuthentication',
    ],
-   # 'DEFAULT_RENDERER_CLASSES': [
-   #    'rest_framework.renderers.JSONRenderer',
-   # ],
+   'DEFAULT_RENDERER_CLASSES': [
+      'rest_framework.renderers.JSONRenderer',
+   ],
    'DEFAULT_FILTER_BACKENDS': [
       'django_filters.rest_framework.DjangoFilterBackend',
    ],

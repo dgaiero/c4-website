8b1541512597d43c8a4f3928433f4f2d169d16b6
Dominic Gaiero <dpgaiero@gmail.com>
2019-06-30 17:15:59 -0700

    reduce default api permissions

--- webserver/c4Lookup/c4Lookup/settings.py
+++ webserver/c4Lookup/c4Lookup/settings.py
@@ -79,9 +79,9 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
    'DEFAULT_PERMISSION_CLASSES': [
-      #  'c4Lookup.SafelistPermission.IsAdminOrReadOnly',
+      'c4Lookup.SafelistPermission.IsAdminOrReadOnly',
       # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-      'rest_framework.permissions.AllowAny',
+      # 'rest_framework.permissions.AllowAny',
    ],
    'DEFAULT_AUTHENTICATION_CLASSES': [
       'rest_framework.authentication.TokenAuthentication',

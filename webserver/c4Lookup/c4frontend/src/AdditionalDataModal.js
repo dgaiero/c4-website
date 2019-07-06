6451adda330d6f53bc33d7ae79d0d623e0ac981a
Dominic Gaiero <dpgaiero@gmail.com>
2019-07-06 08:19:32 -0700

    remove excess code

--- webserver/c4Lookup/c4frontend/src/AdditionalDataModal.js
+++ webserver/c4Lookup/c4frontend/src/AdditionalDataModal.js
@@ -1,92 +1,24 @@
 import React, { Component } from 'react';
-import AsyncSelect from 'react-select/async';
-import axios from "axios";
 import {
-   Container, Col, Form,
-   FormGroup, Label,
-   Button, FormText, Modal, ModalHeader, ModalBody, ModalFooter, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText
+   Button, Modal, ModalHeader, ModalBody, ModalFooter,
 } from 'reactstrap';
 import './Modal.css';
 
 export default class memberSearchModal extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         // selectedActivityKeyword: '',
-         activityKeywords: [],
-
-         topicalKeywords: [],
-
-         selectedUniversities: [],
-         validate: {
-            emailState: '',
-         },
-         selectedOption: null,
-         modal: false
-      }
-      this.handleChange = this.handleChange.bind(this);
-      this.submitForm = this.submitForm.bind(this);
-      // this.toggle = this.toggle.bind(this);
-   }
-
-   submitForm(e) {
-      e.preventDefault();
-      // console.log(`This is a test!`)
-      // console.log(this.state.selectedUniversities)
-      // console.log(this.state.activityKeywords)
-      // console.log(this.state.topicalKeywords)
-      var url = "/api/v1/users/?format=json";
-      for (let i = 0; i < this.state.selectedUniversities.length; i++) {
-            url += '&organization=' + this.state.selectedUniversities[i].value;
-      }
-      for (let i = 0; i < this.state.activityKeywords.length; i++) {
-         url += '&keywords=' + this.state.activityKeywords[i].value;
-      }
-      for (let i = 0; i < this.state.topicalKeywords.length; i++) {
-         url += '&keywords=' + this.state.topicalKeywords[i].value;
-      }
-      // console.log(url);
-      return url;
-   }
-
-   handleChange = (e) => {
-      // console.log(event);
-      let { name, value } = e.target;
-      if (e.target.type === "checkbox") {
-         value = e.target.checked;
-      }
-      const queryData = { ...this.state.queryData, [name]: value };
-      this.setState({ queryData });
-   };
    
    /* Passed in:
       props:
-         // - this.props.modalTitle
-         // - this.props.data = [
-         //    {title: Title, body: Body},
-         //    {title: Title, body: Body},
-         //    etc...
-         // ]
          - title
          - body
     */
 
    render() {
-      const { toggle, size } = this.props;
-
+      const { openStatus, toggle, size } = this.props;
       return (
-         <Modal isOpen={true} toggle={toggle} centered size={size}>
+         <Modal isOpen={openStatus} toggle={toggle} centered size={size}>
             <ModalHeader toggle={toggle}>{this.props.title}</ModalHeader>
             <ModalBody>
                {this.props.body}
-               {/* <ListGroup>
-                  {this.props.data.map((value, index) => {
-                     return <ListGroupItem>
-                        <ListGroupItemHeading>{value.title}</ListGroupItemHeading>
-                        <ListGroupItemText>{value.body}</ListGroupItemText>
-                     </ListGroupItem>
-                  })}
-               </ListGroup> */}
             </ModalBody>
             <ModalFooter>
                <Button color="primary" onClick={toggle}>Close</Button>

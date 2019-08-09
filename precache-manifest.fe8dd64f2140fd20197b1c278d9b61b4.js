e43d37656e6fcad8e99e358301120e9bd05b57a2
Dominic Gaiero <dpgaiero@gmail.com>
2019-08-08 17:20:44 -0700

    Updates

---
+++ precache-manifest.fe8dd64f2140fd20197b1c278d9b61b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd8674be60068918116555b46def477",
+    "url": "/index.html"
+  },
+  {
+    "revision": "218fbfa455ea7745bcf8",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5ab5315cc42ec96c12c9",
+    "url": "/static/css/main.95b33a78.chunk.css"
+  },
+  {
+    "revision": "218fbfa455ea7745bcf8",
+    "url": "/static/js/2.1e6a74b1.chunk.js"
+  },
+  {
+    "revision": "5ab5315cc42ec96c12c9",
+    "url": "/static/js/main.a6392520.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

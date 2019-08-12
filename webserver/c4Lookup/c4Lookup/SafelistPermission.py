1b5dd4f35bd0feaedbb657f6329c2fadfaedd69f
Dominic Gaiero <dpgaiero@gmail.com>
2019-08-11 18:32:00 -0700

    update safelist permission

--- webserver/c4Lookup/c4Lookup/SafelistPermission.py
+++ webserver/c4Lookup/c4Lookup/SafelistPermission.py
@@ -1,30 +1,31 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework import permissions
 from django.conf import settings
+import sys
 
 class SafelistPermission(permissions.BasePermission):
-    """
-    Ensure the request's IP address is on the safe list configured in Django settings.
-    """
+   """
+   Ensure the request's IP address is on the safe list configured in Django settings.
+   """
 
-    def has_permission(self, request, view):
+   def has_permission(self, request, view):
 
-        remote_addr = request.META['REMOTE_ADDR']
-        print(remote_addr)
+      remote_addr = request.META['REMOTE_ADDR']
+      sys.stderr.write(remote_addr)
 
-        for valid_ip in settings.REST_SAFE_LIST_IPS:
-            if remote_addr == valid_ip or remote_addr.startswith(valid_ip):
-                return True
+      for valid_ip in settings.REST_SAFE_LIST_IPS:
+         if remote_addr == valid_ip or remote_addr.startswith(valid_ip):
+            return True
 
-        return False
+      return False
 
 
 class IsAdminOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        else:
-            return request.user.is_staff
+   def has_permission(self, request, view):
+      if request.method in SAFE_METHODS:
+         return True
+      else:
+         return request.user.is_staff
 
 class NoAccess(BasePermission):
    def has_permission(self, request, view):

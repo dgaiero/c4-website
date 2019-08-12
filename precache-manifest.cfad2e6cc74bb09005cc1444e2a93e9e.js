b3fb3eb2a441837266dbfb5c24c290507eb8b27d
Dominic Gaiero <dpgaiero@gmail.com>
2019-08-11 19:34:26 -0700

    Updates

--- precache-manifest.cfad2e6cc74bb09005cc1444e2a93e9e.js
+++ precache-manifest.cfad2e6cc74bb09005cc1444e2a93e9e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46d298ae0f6d6c7e1d0658fabe38248a",
+    "revision": "5a0271dc229cf5208469819b9fbdd875",
     "url": "/c4-website/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/c4-website/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "0c74779a78b0a6e03676",
+    "revision": "f79b6e1dac4e8b78e259",
     "url": "/c4-website/static/css/main.95b33a78.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/c4-website/static/js/2.76fce93b.chunk.js"
   },
   {
-    "revision": "0c74779a78b0a6e03676",
-    "url": "/c4-website/static/js/main.e4b05857.chunk.js"
+    "revision": "f79b6e1dac4e8b78e259",
+    "url": "/c4-website/static/js/main.b2e85365.chunk.js"
   },
   {
     "revision": "719a68a9ed9e3dea1595",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/c4-website/precache-manifest.6ece9301a4e8e5732d1b23bfd9dd864e.js"
+  "/c4-website/precache-manifest.cfad2e6cc74bb09005cc1444e2a93e9e.js"
 );
 
 self.addEventListener('message', (event) => {

8fe9b88448a5317b85874148db20aeae3d6cc7b2
Dominic Gaiero <dpgaiero@gmail.com>
2019-08-24 17:33:15 -0700

    added 404

---
+++ webserver/c4Lookup/c4frontend/src/404.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {
+   Container,
+   Button,
+   Jumbotron,
+} from 'reactstrap';
+import './App.css';
+import { withRouter } from 'react-router-dom';
+
+class NotFound extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+
+         navIsOpen: false,
+         devModeOpen: true,
+      };
+   }
+   render() {
+      const { pathname } = this.props.location;
+      return (
+
+         <Jumbotron fluid>
+            <Container fluid>
+               <h1 className="display-3">404</h1>
+               <p className="lead">Page not found</p>
+               <hr className="my-2" />
+               <p>The requested URL was not found on this server</p>
+               <p className="lead">
+                  The path <code>{pathname}</code> could not be found on this server.
+               </p>
+            </Container>
+         </Jumbotron>
+      );
+   }
+}
+
+export default NotFound

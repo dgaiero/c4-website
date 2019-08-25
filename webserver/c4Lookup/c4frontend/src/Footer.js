70b1dc6fa18ff95bcc7568f46c268afa3bd5b009
Dominic Gaiero <dpgaiero@gmail.com>
2019-08-24 17:34:19 -0700

    updated footer to include icon

--- webserver/c4Lookup/c4frontend/src/Footer.js
+++ webserver/c4Lookup/c4frontend/src/Footer.js
@@ -4,6 +4,8 @@ import {
 } from 'reactstrap';
 
 import './App.css';
+import { NBSP } from './helper';
+import './footer.css'
 
 class Footer extends Component {
    render() {
@@ -11,7 +13,14 @@ class Footer extends Component {
          <footer className="footer mt-auto py-3" style={{ backgroundColor: '#F8F9FA' }}>
             <Container fluid className="clearfix">
                <span className="text-muted float-left">Copyright {'\u00A9'} 2019 Central Coast Climate Collaborative </span>
-               <a href="https://www.centralcoastclimate.org/" className="float-right" target="_blank" rel="noopener noreferrer">Visit main website</a>
+               {/* <div className="float-right"> */}
+                     {NBSP}
+               <div className="float-right">
+               <p className="c4-icon text-muted"></p>
+               {NBSP}
+               <a href="https://www.centralcoastclimate.org/" target="_blank" rel="noopener noreferrer">Visit main website</a>             
+               </div>
+               {/* </div> */}
             </Container>
          </footer>
       )

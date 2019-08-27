c69b3be3f7b55945a36b2b139d43ab4e2e6bfb6e
Dominic Gaiero <dpgaiero@gmail.com>
2019-08-26 20:25:15 -0700

    update copy to clipboard

--- webserver/c4Lookup/c4frontend/src/copyQueryToClipboard.js
+++ webserver/c4Lookup/c4frontend/src/copyQueryToClipboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-   Button, Modal, ModalHeader, ModalBody, ModalFooter, InputGroup, InputGroupAddon, Input, Tooltip 
+    Button, Modal, ModalHeader, ModalBody, ModalFooter, InputGroup, InputGroupAddon, Input, UncontrolledPopover, PopoverHeader, PopoverBody
 } from 'reactstrap';
 import './Modal.css';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
@@ -11,8 +11,8 @@ export default class CopyQueryToClipboard extends Component {
    constructor(props) {
       super(props);
       this.state = {
-         copyURL: false,
-         copyURLTooltip: false,
+          copyCode: false,
+          copyURL: false,
       }
    }
    /* Passed in:
@@ -23,6 +23,10 @@ export default class CopyQueryToClipboard extends Component {
 
    render() {
       const { openStatus, toggle, size, query } = this.props;
+       const protocol = window.location.protocol;
+       const host = window.location.host;
+       const encodedQuery = base64.encode(query)
+       const URL = protocol.concat("//",host,"/",encodedQuery);
       return (
          <Modal isOpen={openStatus} toggle={toggle} centered size={size}>
             <ModalHeader toggle={toggle}>Copy Query to Clipboard</ModalHeader>
@@ -30,22 +34,39 @@ export default class CopyQueryToClipboard extends Component {
                Saving or sharing a query is easy! You can copy the code below to
                your clipboard and paste it in on this page later, or you can copy
                the sharable URL.< br />
-               <b>Copy URL to clipboard</b>
+               <b>Copy code to clipboard</b>
 
                <InputGroup>
-                  <Input disabled placeholder={base64.encode(query)}/>
+                  <Input disabled placeholder={encodedQuery}/>
                   <InputGroupAddon addonType="append">
 
-                     <CopyToClipboard text={base64.encode(query)}
-                        onCopy={() => this.setState({ copyURL: true })}>
-                        <Button id="copyURLToClipboard">Copy to clipboard</Button>
+                          <CopyToClipboard text={encodedQuery}
+                        onCopy={() => this.setState({ copyCode: true })}>
+                              <Button id="copyCodeToClipboard" color="success">Copy to clipboard</Button>
                      </CopyToClipboard>
-                     <Tooltip placement="top" isOpen={this.state.copyURL} autohide={false} target="copyURLToClipboard">
-                        Try to select this text!
-                     </Tooltip>
+                        <UncontrolledPopover trigger="focus" placement="bottom" target="copyCodeToClipboard">
+                        <PopoverHeader>Copied Code to Clipboard</PopoverHeader>
+                        <PopoverBody>You can share or save this code to return to this query later!<br /><kbd>Ctrl</kbd> + <kbd>V</kbd> can retrieve the code from your clipboard.</PopoverBody>
+                    </UncontrolledPopover>
                   </InputGroupAddon>
                </InputGroup>
-               {query}
+                <br />
+                <b>Want to Copy a URL instead?</b>
+
+                <InputGroup>
+                      <Input disabled placeholder={URL} />
+                    <InputGroupAddon addonType="append">
+
+                          <CopyToClipboard text={URL}
+                            onCopy={() => this.setState({ copyURL: true })}>
+                              <Button id="copyURLToClipboard" color="success">Copy to clipboard</Button>
+                        </CopyToClipboard>
+                        <UncontrolledPopover trigger="focus" placement="bottom" target="copyURLToClipboard">
+                            <PopoverHeader>Copied URL to Clipboard</PopoverHeader>
+                            <PopoverBody>You can share or save this URL to return to this query later!<br /><kbd>Ctrl</kbd> + <kbd>V</kbd> can retrieve the code from your clipboard.</PopoverBody>
+                        </UncontrolledPopover>
+                    </InputGroupAddon>
+                </InputGroup>
             </ModalBody>
             <ModalFooter>
                <Button color="primary" onClick={toggle}>Close</Button>

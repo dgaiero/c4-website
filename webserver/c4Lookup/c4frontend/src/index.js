6020e95de3b9510a690e2f0847c321d03101bc4b
Dominic Gaiero <dpgaiero@gmail.com>
2019-09-15 10:40:11 -0700

    add Provider for Footer to make redux store work

--- webserver/c4Lookup/c4frontend/src/index.js
+++ webserver/c4Lookup/c4frontend/src/index.js
@@ -7,6 +7,7 @@ import Footer from './Footer'
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk'; 
+import { Provider } from 'react-redux';
 
 import unregisterServiceWorker from './registerServiceWorker';
 
@@ -14,5 +15,5 @@ const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(rootReducer);
 
 ReactDOM.render(
-   [<App store={store} key={1} />, <Footer key="2" />], document.getElementById('root'));
+   [<App store={store} key={1} />, <Provider store={store} key={2} ><Footer store={store}/></Provider>], document.getElementById('root'));
 unregisterServiceWorker();

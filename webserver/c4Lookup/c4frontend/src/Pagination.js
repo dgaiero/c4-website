4242bea556488771ff3bdb708dc8c955ac42941c
Dominic Gaiero <dpgaiero@gmail.com>
2019-09-15 10:29:02 -0700

    resolved #18 Changed componentWillMount to componentDidMount componentWillMount is deprecated

--- webserver/c4Lookup/c4frontend/src/Pagination.js
+++ webserver/c4Lookup/c4frontend/src/Pagination.js
@@ -47,7 +47,7 @@ class PaginationWrapper extends React.Component {
       this.state = { pager: {} };
    }
 
-   componentWillMount() {
+   componentDidMount() {
       // set page if items array isn't empty
       if (this.props.items && this.props.items.length) {
          this.setPage(this.props.initialPage);

ee0da74abaa558b1434239de7bac98fa112ceffd
Dominic Gaiero <dpgaiero@gmail.com>
2019-11-26 08:27:13 -0800

    update new features background

--- webserver/c4Lookup/c4frontend/src/indexPage.js
+++ webserver/c4Lookup/c4frontend/src/indexPage.js
@@ -20,46 +20,42 @@ import { connect } from 'react-redux';
 const UnderConstructionCard = () => {
    return (
       <Card className="cardHideOverflow card-no-border shadow text-light">
-            <div className="card-defaults">
-                <span className="newFeaturesBanner"><b>New Features Coming Soon!</b></span>
-                <div className="underConstructionCardImage">
-                <CardBody>
-                    <h5 className='cardTitle'>Under Construction</h5>
-                    <CardText>New features will be coming soon!</CardText>
-                </CardBody>
-                <div className="card-footer-light"><Button color="light" disabled>Button Coming Soon</Button></div>
-                </div>
+            <span className="newFeaturesBanner"><b>New Features Coming Soon!</b></span>
+            <div className="underConstructionCardImage card-defaults">
+               <CardBody>
+                  <h5 className='cardTitle'>Under Construction</h5>
+                  <CardText>New features will be coming soon!</CardText>
+               </CardBody>
+               <div className="card-footer-light"><Button color="light" disabled>Button Coming Soon</Button></div>
             </div>
-         </Card>
+      </Card>
    )
 }
 
 const LookingForUnivCollaboratorCard = () => {
    return (
       <Card className="cardHideOverflow card-no-border shadow text-light" >
-                <div className="lookingForUnivCollaboratorCardImage card-defaults">
-                <CardBody>
-                    <h5 className='cardTitle'>Looking for a University Collaborator?</h5>
-                    <CardText>Interested in finding faculty, staff, or students to work on a project?</CardText>
-
-                </CardBody>
+         <div className="lookingForUnivCollaboratorCardImage card-defaults">
+            <CardBody>
+               <h5 className='cardTitle'>Looking for a University Collaborator?</h5>
+               <CardText>Interested in finding faculty, staff, or students to work on a project?</CardText>
+            </CardBody>
             <div className="card-footer-light"><Button color="light" tag={RRNavLink} exact to="/univCollaborator">Search For a University Collaborator</Button></div>
-                </div>
-         </Card>
+         </div>
+      </Card>
    )
 }
 
 const LookingFor4CCollaboratorCard = () => {
    return (
       <Card className="cardHideOverflow card-no-border shadow text-light" >
-            <div className="lookingForGovCollaboratorCardImage card-defaults">
-               <CardBody>
-                  <h5 className='cardTitle'>Looking for a 4C Collaborator?</h5>
-                  <CardText>Interested in finding a city, county, NGO, or regional staff?</CardText>
-
-               </CardBody>
+         <div className="lookingForGovCollaboratorCardImage card-defaults">
+            <CardBody>
+               <h5 className='cardTitle'>Looking for a 4C Collaborator?</h5>
+               <CardText>Interested in finding a city, county, NGO, or regional staff?</CardText>
+            </CardBody>
             <div className="card-footer-light"><Button color="light" tag={RRNavLink} exact to="/4CCollaborator">Search For a 4C Collaborator</Button></div>
-            </div>
+         </div>
       </Card>
    )
 }
@@ -78,24 +74,23 @@ class FrontPageCards extends Component {
    PasteQCode = () => {
       return (
          <Card className="cardHideOverflow card-no-border shadow text-light" >
-               <div className="PasteQCodeCardImage card-defaults">
-                  <CardBody>
-                     <h5 className='cardTitle'>Have a saved query?</h5>
-                     <CardText>If you have a saved code, you can retrieve it here.</CardText>
-
-                  </CardBody>
-                  <div className="card-footer-light"><Button color="light" onClick={() => this.setState({ pasteFromClipBoardToggle: !this.state.pasteFromClipBoardToggle })}>Paste Code</Button></div>
-                  <PasteFromCipboardModal
-                     openStatus={this.state.pasteFromClipBoardToggle}
-                     endpoint="univCollaborator"
-                     history={this.props.history}
-                     toggle={() => this.setState(
-                        {
-                           pasteFromClipBoardToggle: !this.state.pasteFromClipBoardToggle
-                        })}
-                     size='lg'
-                  />
-               </div>
+            <div className="PasteQCodeCardImage card-defaults">
+               <CardBody>
+                  <h5 className='cardTitle'>Have a saved query?</h5>
+                  <CardText>If you have a saved code, you can retrieve it here.</CardText>
+               </CardBody>
+               <div className="card-footer-light"><Button color="light" onClick={() => this.setState({ pasteFromClipBoardToggle: !this.state.pasteFromClipBoardToggle })}>Paste Code</Button></div>
+               <PasteFromCipboardModal
+                  openStatus={this.state.pasteFromClipBoardToggle}
+                  endpoint="univCollaborator"
+                  history={this.props.history}
+                  toggle={() => this.setState(
+                     {
+                        pasteFromClipBoardToggle: !this.state.pasteFromClipBoardToggle
+                     })}
+                  size='lg'
+               />
+            </div>
          </Card>
       )
    }
@@ -104,8 +99,8 @@ class FrontPageCards extends Component {
    render() {
       return (
          <>
-         <Title name="Home" />
-         <WelcomeJumbotron />
+            <Title name="Home" />
+            <WelcomeJumbotron />
             <Container>
                <CardDeck className="card-deck-overrides">
                   <LookingForUnivCollaboratorCard />
@@ -114,7 +109,7 @@ class FrontPageCards extends Component {
                   {/* <this.PasteQCode /> */}
                </CardDeck>
             </Container>
-        </>
+         </>
       );
    }
 }

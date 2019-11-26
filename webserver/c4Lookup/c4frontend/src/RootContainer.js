f497e43e5ebd6bd3d6cdbf2f18116891d199be12
Dominic Gaiero <dpgaiero@gmail.com>
2019-11-25 16:25:26 -0800

    changed /save route to /ComingSoon route

--- webserver/c4Lookup/c4frontend/src/RootContainer.js
+++ webserver/c4Lookup/c4frontend/src/RootContainer.js
@@ -4,19 +4,20 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
 
 import C4TableView from './4CCollaborators/TableView'
+import ComingSoon from './ComingSoon'
 import FrontPageCards from './indexPage';
 import Loader from './loader';
 import Loading from './Loading'
 import Navigation from './Navigation'
 import NotFound from './404'
-import ParseSaveQuery from './ParseSaveQuery'
 import UnivTableView from './universityCollaborators/TableView'
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { fetchSettings } from './actions/settingsActions'
 
+// import ParseSaveQuery from './ParseSaveQuery'
 // import DebugMessage from './DebugMessage'
-// import ComingSoon from './ComingSoon'
+
 
 axios.defaults.baseURL = "https://api.centralcoastclimate.org"
 // axios.defaults.baseURL = "http://192.168.86.218:8000"
@@ -39,7 +40,7 @@ class RootContainer extends Component {
             <Navigation />
             <Switch>
                <Route path="/" exact component={FrontPageCards} />
-               <Route path="/save/:id?" component={ParseSaveQuery} />
+               <Route path="/save/:id?" component={ComingSoon} />
                <Route path="/univCollaborator/" exact component={UnivTableView} />
                <Route path="/4CCollaborator/" exact component={C4TableView} />
                <Route component={NotFound} status={404} />

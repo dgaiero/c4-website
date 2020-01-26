04b84468a6ff42f82b4690ac5698b42065b59769
Dominic Gaiero <dpgaiero@gmail.com>
2020-01-25 22:30:20 -0800

    add google analytics to website

--- webserver/c4Lookup/c4frontend/src/App.js
+++ webserver/c4Lookup/c4frontend/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 
-import { Route, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Router } from 'react-router-dom'
 
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux';
 import React from 'react';
+import ReactGA from 'react-ga';
 import RootContainer from './RootContainer';
+import { createBrowserHistory } from 'history';
+
+const trackingId = "UA-156443950-1";
+ReactGA.initialize(trackingId);
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+   ReactGA.set({ page: location.pathname }); // Update the user's current page
+   ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 const App = ({ store }) => (
    <Provider store={store}>
-      <Router>
+      <Router history={history}>
          <Route path="/:filter?" component={RootContainer} />
       </Router>
    </Provider>

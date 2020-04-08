8b2a1c1f44c282deea8d0a0128f28099e1fad8ba
Dominic Gaiero <dpgaiero@gmail.com>
2020-04-07 17:15:04 -0700

    Updates

---
+++ precache-manifest.e6b91c9b13063f1b05964803975dec5a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391e19f94d039d17b1a8937e5386a712",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c54d4d80ca10557f90b3",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "3c8e57504d4dc12a03d5",
+    "url": "/static/css/main.028a8496.chunk.css"
+  },
+  {
+    "revision": "c54d4d80ca10557f90b3",
+    "url": "/static/js/2.cb48e3d7.chunk.js"
+  },
+  {
+    "revision": "3c8e57504d4dc12a03d5",
+    "url": "/static/js/main.8df81efd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "496a798ecd5ac4180ab3fec179ba3599",
+    "url": "/static/media/c4-icon.496a798e.svg"
+  },
+  {
+    "revision": "55aee19717731f3c5a92aafde2f4d87a",
+    "url": "/static/media/c4-icon.55aee197.woff"
+  },
+  {
+    "revision": "7623e0d49f4a05687acc62c39cfb6b7d",
+    "url": "/static/media/c4-icon.7623e0d4.ttf"
+  },
+  {
+    "revision": "8c8da8e6841def22e7999911c3a8714a",
+    "url": "/static/media/c4-icon.8c8da8e6.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1560ed484fe8a9a805996180459e8bed.js"
+  "/precache-manifest.e6b91c9b13063f1b05964803975dec5a.js"
 );
 
 self.addEventListener('message', (event) => {

dc94275d57b5ba2ca5f7ae1ec6007d6b5f27b89c
Quentin Monasterial <quinmona98@gmail.com>
2021-01-18 16:16:12 -0800

    Updates

---
+++ precache-manifest.eb623cbfc33600c545310d9392420d99.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ee23d5bc9033b1ea5529046bae33f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "355d0ee465efa968a9de",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c8e9969838f54e8847b4",
+    "url": "/static/css/main.b37c548c.chunk.css"
+  },
+  {
+    "revision": "355d0ee465efa968a9de",
+    "url": "/static/js/2.d810eeec.chunk.js"
+  },
+  {
+    "revision": "c8e9969838f54e8847b4",
+    "url": "/static/js/main.de678542.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "55aee19717731f3c5a92aafde2f4d87a",
+    "url": "/static/media/c4-icon.55aee197.woff"
+  },
+  {
+    "revision": "7623e0d49f4a05687acc62c39cfb6b7d",
+    "url": "/static/media/c4-icon.7623e0d4.ttf"
+  },
+  {
+    "revision": "8c8da8e6841def22e7999911c3a8714a",
+    "url": "/static/media/c4-icon.8c8da8e6.eot"
+  },
+  {
+    "revision": "e27623727a40ddb6bd0633fd65fa5518",
+    "url": "/static/media/c4-icon.e2762372.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fb5ba681f007338884e1adbd49e2675.js"
+  "/precache-manifest.eb623cbfc33600c545310d9392420d99.js"
 );
 
 self.addEventListener('message', (event) => {
